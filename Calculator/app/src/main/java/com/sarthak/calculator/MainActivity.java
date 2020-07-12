178f832e47069a94667b00a5fba321b08c770c08
Sarthak Sahu <32789001+Sarthak99@users.noreply.github.com>
2020-07-12 11:56:16 +0530

    Enhanced landscape layout

--- Calculator/app/src/main/java/com/sarthak/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/sarthak/calculator/MainActivity.java
@@ -1,14 +1,146 @@
 package com.sarthak.calculator;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText result;
+    private EditText newEntry;
+    private TextView displayOperation;
+
+    private Double operand1 = null;
+    private String pendingOperation = "=";
+
+    private static final String STATE_PENDING_OPERATIONS = "pendingOperation";
+    private static final String STATE_PENDING_OPERAND = "operand1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        newEntry = findViewById(R.id.newEntry);
+        displayOperation = findViewById(R.id.operation);
+
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button buttonDot = findViewById(R.id.buttonDecimal);
+
+        Button buttonAdd = findViewById(R.id.buttonAdd);
+        Button buttonSubtract = findViewById(R.id.buttonSubtract);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonDivide = findViewById(R.id.buttonDivide);
+        Button buttonEquals = findViewById(R.id.buttonEqual);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                newEntry.append(b.getText().toString());
+            }
+        };
+
+        button0.setOnClickListener(listener);
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+        button8.setOnClickListener(listener);
+        button9.setOnClickListener(listener);
+        buttonDot.setOnClickListener(listener);
+
+        View.OnClickListener opListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                String op = b.getText().toString();
+                String value = newEntry.getText().toString();
+                if (value.length() != 0) {
+                    try {
+                        Double doubleValue = Double.valueOf(value);
+                        performOperation(doubleValue, op);
+                    } catch (NumberFormatException n) {
+                        newEntry.setText("");
+                    }
+                }
+                pendingOperation = op;
+                displayOperation.setText(pendingOperation);
+            }
+        };
+
+        buttonAdd.setOnClickListener(opListener);
+        buttonSubtract.setOnClickListener(opListener);
+        buttonMultiply.setOnClickListener(opListener);
+        buttonDivide.setOnClickListener(opListener);
+        buttonEquals.setOnClickListener(opListener);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(STATE_PENDING_OPERATIONS, pendingOperation);
+        if(operand1 != null){
+            outState.putDouble(STATE_PENDING_OPERAND, operand1);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATIONS);
+        operand1 = savedInstanceState.getDouble(STATE_PENDING_OPERAND);
+        displayOperation.setText(pendingOperation);
+    }
+
+    private void performOperation(Double value, String operation) {
+
+        if (operand1 == null) {
+            operand1 = Double.valueOf(value);
+        } else {
+            if (pendingOperation.equals("=")) {
+                pendingOperation = operation;
+            }
+            switch (pendingOperation) {
+                case "=":
+                    operand1 = value;
+                    break;
+                case "+":
+                    operand1 += value;
+                    break;
+                case "-":
+                    operand1 -= value;
+                    break;
+                case "*":
+                    operand1 = operand1 * value;
+                    break;
+                case "/":
+                    if (value == 0) {
+                        operand1 = 0.0;
+                    } else {
+                        operand1 = operand1 / value;
+                    }
+                    break;
+            }
+        }
+        result.setText(operand1.toString());
+        newEntry.setText("");
     }
 }
